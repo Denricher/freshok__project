611d387834bcc712baa1e773afa896c754cf7c11
Denis Yakovlev <yakovlev.denrich@gmail.com>
2021-07-31 20:49:58 +0500

    finished header, slider, product, footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,13 @@
-$(function () { });
+$(function () {
+
+	$('.slider__items').slick({
+		arrows: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt="arrow-left"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt="arrow-right"></button>',
+		// autoplay: true,
+		// autoplaySpeed: 3000,
+	});
+
+	var mixer = mixitup('.product__items');
+
+});
