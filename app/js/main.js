98972d80a175656170bb2be8422440d12c8fe5c4
Denis Yakovlev <yakovlev.denrich@gmail.com>
2021-08-07 16:00:18 +0500

    Первая часть работы

--- app/js/main.js
+++ app/js/main.js
@@ -4,11 +4,17 @@ $(function () {
 		arrows: true,
 		prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt="arrow-left"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt="arrow-right"></button>',
-		// autoplay: true,
-		// autoplaySpeed: 3000,
 	});
 
-	var mixer = mixitup('.product__items');
-	var mixer = mixitup('.promo__items');
-	//что-то не работает
+	var containerEl1 = document.querySelector('[data-ref="container-1"]');
+	var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+	var config = {
+		controls: {
+			scope: 'local'
+		}
+	};
+
+	var mixer1 = mixitup(containerEl1, config);
+	var mixer2 = mixitup(containerEl2, config);
 });
